15587ef9aa6a44a7dbb055c8480092dfeb3ccb4b
madisonmalachi <madisonmalachi@172.17.5.79>
2024-08-29 22:35:54 -0400

    Third commit message

--- LSP_Fall_2024/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ LSP_Fall_2024/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -6,7 +6,7 @@ package org.howard.edu.lsp.assignment1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World !!"); 
+		System.out.println("Hello World!!"); 
 	}
 
 }
