6d27e28c2a1895bc9f3925af13e9348eb82397ee
madisonmalachi <madisonmalachi@localhost>
2024-09-12 21:06:19 -0400

    this is the first commit

--- LSP_Fall_2024/src/org/howard/edu/lsp/assignment2/WordCounting.java
+++ LSP_Fall_2024/src/org/howard/edu/lsp/assignment2/WordCounting.java
@@ -1,18 +1,45 @@
 /**
- * 
+ * Name: Madison Malachi
  */
 package org.howard.edu.lsp.assignment2;
 
-/**
- * 
- */
-public class WordCounting {
+import java.util.HashMap;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-	/**
-	 * @param args
-	 */
+
+
+
+
+public class WordCounting {
 	public static void main(String[] args) {
+		HashMap<String, Integer>WordsCountHash = new HashMap<String, Integer>();
+		String filePath = "src/org/howard/edu/lsp/assignment2/words.text";
+		
+		try {
+			File file = new File(filePath);
+			Scanner scanner = new Scanner(file);
+			
+			while (scanner.hasNext()) {
+				String line = scanner.nextLine();
+				line = line.toLowerCase(); // making letters are all lowercase
+				String[] words = line.split("[^a-z]+"); // there needs to be a split with the regex
+				
+			    for (String word : words) {
+			    	if(!word.isEmpty() && word.length() > 3) {
+			    		WordsCountHash.put(word,  WordsCountHash.getOrDefault(word, 0) +1);
+			    		
+			    	}
+			    }
+			}
+			scanner.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found: " + e.getMessage());	
+		}
+		System.out.println(WordsCountHash);
 		// TODO Auto-generated method stub
+		
 
 	}
 
