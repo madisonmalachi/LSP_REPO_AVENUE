c7296cf05a5b47d947a377755e252584a7b8e74a
madisonmalachi <madisonmalachi@localhost>
2024-09-12 21:05:56 -0400

    This is the first commit

---
+++ LSP_Fall_2024/src/org/howard/edu/lsp/assignment2/WordCounting.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.howard.edu.lsp.assignment2;
+
+/**
+ * 
+ */
+public class WordCounting {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
