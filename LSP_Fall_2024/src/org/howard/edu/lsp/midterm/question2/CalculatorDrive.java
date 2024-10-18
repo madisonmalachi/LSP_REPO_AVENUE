47e138dcede82c972b0e610d546562e96bf5a9ee
madisonmalachi <madisonmalachi@172.17.5.79>
2024-10-17 20:42:51 -0400

    first try

--- LSP_Fall_2024/src/org/howard/edu/lsp/midterm/question2/CalculatorDrive.java
+++ LSP_Fall_2024/src/org/howard/edu/lsp/midterm/question2/CalculatorDrive.java
@@ -3,6 +3,9 @@ package org.howard.edu.lsp.midterm.question2;
 public class CalculatorDrive {
 
 	public static void main(String[] args) {
+		System.out.println(Calculatorclass.sum(5, 10)); // Output: 15
+        System.out.println(Calculatorclass.sum(3.5, 7.8)); // Output: 11.3
+        System.out.println(Calculatorclass.sum(new int[]{1, 2, 3, 4, 5})); // Output: 15
 		// TODO Auto-generated method stub
 
 	}
