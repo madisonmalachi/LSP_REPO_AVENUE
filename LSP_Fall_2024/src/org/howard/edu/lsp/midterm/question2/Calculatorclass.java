6240f079be18386ba3108bb85aa653b004ed551f
madisonmalachi <madisonmalachi@172.17.5.79>
2024-10-17 21:01:42 -0400

    first commit

--- LSP_Fall_2024/src/org/howard/edu/lsp/midterm/question2/Calculatorclass.java
+++ LSP_Fall_2024/src/org/howard/edu/lsp/midterm/question2/Calculatorclass.java
@@ -1,10 +1,26 @@
 package org.howard.edu.lsp.midterm.question2;
 
+/** utility class provides methods to sum numbers of different types.
+ * the source: https://www.geeksforgeeks.org/java/
+ */
+
 public class Calculatorclass {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	// sum of two integers 
+	
+	public static int sum(int num1, int num2) {
+		return num1 + num2;
 	}
-
+	// sum of two double values 
+	public static double sum(double num1, double num2) {
+		return num1 + num2;
+	}
+	// the sum of all elements in the array
+	public static int sum(int[] nums) {
+		int total = 0;
+		for (int num: nums) {
+			total += num;
+			
+		}
+		return total;
+	}
 }
