44cf096faaac98070165ec43a1f4c0cab8f9c3f9
madisonmalachi <madisonmalachi@172.17.5.79>
2024-10-17 20:15:56 -0400

    first commit

---
+++ LSP_Fall_2024/src/org/howard/edu/lsp/midterm/question1/BookClass.java
@@ -0,0 +1,15 @@
+package org.howard.edu.lsp.midterm.question1;
+
+public class BookClass {
+	private String title;
+	private String author;
+	private String ISBN;
+	private int yearPublished; 
+public 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
