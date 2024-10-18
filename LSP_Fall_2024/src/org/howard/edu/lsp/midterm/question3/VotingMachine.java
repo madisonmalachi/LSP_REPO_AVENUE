cda2534f464db4a5484f4a185ed9575c32fe5fce
madisonmalachi <madisonmalachi@172.17.5.79>
2024-10-17 21:02:16 -0400

    first commit

--- LSP_Fall_2024/src/org/howard/edu/lsp/midterm/question3/VotingMachine.java
+++ LSP_Fall_2024/src/org/howard/edu/lsp/midterm/question3/VotingMachine.java
@@ -1,10 +1,44 @@
+
+
+
+
 package org.howard.edu.lsp.midterm.question3;
+/**
+ * This class represents a simplified voting machine that keeps track of candidates and votes.
+ * the source i use:  https://www.geeksforgeeks.org/java/
+ 
+ */
+
+import java.util.HashMap;
 
 public class VotingMachine {
+	private HashMap<String, Integer> candidates = new HashMap<>();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    // Add a candidate to the list
+    public void addCandidate(String name) {
+        candidates.put(name, 0);
+    }
 
-	}
+    // Cast a vote to the candidate with the specified name
+    public void castVote(String name) {
+        if (candidates.containsKey(name)) {
+            candidates.put(name, candidates.get(name) + 1);
+        }
+    }
 
+    // Override toString method to print out the number of votes each candidate has received
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        for (String name : candidates.keySet()) {
+            result.append(name).append(": ").append(candidates.get(name)).append(" votes\n");
+        }
+        return result.toString();
+    }
 }
+
+
+	
+	
+
+
