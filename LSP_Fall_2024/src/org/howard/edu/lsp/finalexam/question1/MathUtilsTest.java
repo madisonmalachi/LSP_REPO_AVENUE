dd56f81f091bfe47e924d4aeeeac3eb0b42a9517
madisonmalachi <madisonmalachi@localhost>
2024-12-05 19:24:05 -0500

    Test 1

---
+++ LSP_Fall_2024/src/org/howard/edu/lsp/finalexam/question1/MathUtilsTest.java
@@ -0,0 +1,14 @@
+package org.howard.edu.lsp.finalexam.question1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathUtilsTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
